ec4e6c50d38e83a9d9560f01ed8510fcb3f97412
shashinherath <shashin.malinda@gmail.com>
2024-05-05 10:56:07 +0530

    create models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_Name = models.CharField(max_length=100)
+    last_Name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    address = models.CharField(max_length=100)
+    birth_Date = models.DateField()
+    contact = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+
+class Lecturer(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_Name = models.CharField(max_length=100)
+    last_Name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+class Course(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
