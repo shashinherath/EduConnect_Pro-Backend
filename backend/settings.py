c13eb391d86fd54e03e3e80b8bb2a587144f56d9
shashinherath <shashin.malinda@gmail.com>
2024-05-13 12:53:00 +0530

    Fix admin app

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'admin',
+    'admin_panel',
     'rest_framework',
     'rest_framework.authtoken',
 ]
