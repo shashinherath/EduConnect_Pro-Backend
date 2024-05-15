4fcd6c9e694ba4eb98c9de55f085ca5282e6ab41
shashinherath <shashin.malinda@gmail.com>
2024-05-15 13:04:48 +0530

    Fix urls

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,10 +1,7 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from .views import UserViewSet
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
-
 urlpatterns = [
-    path('users/', include(router.urls)),
+    path('users/', UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list'),
+    path('users/<int:pk>/', UserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='user-detail'),
 ]
--- backend/urls.py
+++ backend/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('main.urls')),
+    path('api/', include('admin_panel.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
