0a5a70d1c11a06b0294f2463e6d720447f160b87
shashinherath <shashin.malinda@gmail.com>
2024-06-12 14:32:22 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -19,7 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/main/', include('main.urls')),
-    path('api/admin/', include('admin_api.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
