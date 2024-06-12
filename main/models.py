ad1ba2040f6d79dba30b0c81ca7393d40e4084e4
shashinherath <shashin.malinda@gmail.com>
2024-06-12 14:32:29 +0530

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,5 +1,118 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 # Create your models here.
+class Admin(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+class Lecturer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+class Course(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    lecturer_id = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+class Student(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255)
+    profile_pic = models.ImageField(upload_to='images/')
+    course_id = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+#lecture able to chat with student. so create chat model here
+class Chat(models.Model):
+    id = models.AutoField(primary_key=True)
+    message = models.TextField()
+    sender_id = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
+    receiver_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+#lecture able to mark attendence of students. so create attendence model here
+class Attendence(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    lecture_id = models.ForeignKey(Lecturer, on_delete=models.DO_NOTHING)
+    student_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+#lecture able to give lecture material(lecture pdfs videos) to students under the course. so create lecture material model here
+class LectureMaterial(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    file = models.FileField(upload_to='files/')
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+#lecture able to give assignment to students under the course. so create assignment model here
+class Assignment(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    file = models.FileField(upload_to='files/')
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+#student able to submit assignment to lecture under the course. so create assignment submission model here
+class AssignmentSubmission(models.Model):
+    id = models.AutoField(primary_key=True)
+    file = models.FileField(upload_to='files/')
+    assignment_id = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+#student able to view availability of lecturers. lecturers mark their abalaibility by clicking on available or not available button. so create availability model here
+class LecturerAvailability(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    status = models.CharField(max_length=255)
+    lecture_id = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+#students able to use and find knowladge using integrated chatGPT via API. so I need to store those data for each student. so create chatGPT model here
+class ChatGPT(models.Model):
+    id = models.AutoField(primary_key=True)
+    message = models.TextField()
+    response = models.TextField()
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
