28b32e42ddb329613ce3b8f64208c8dd6389a9bd
shashinherath <shashin.malinda@gmail.com>
2024-06-12 14:32:18 +0530

    Update serializers.py

--- main/serializers.py
+++ main/serializers.py
@@ -1,7 +1,2 @@
 from rest_framework import serializers
 from . import models
-
-class LecturerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Lecturer
-        fields = '__all__'
