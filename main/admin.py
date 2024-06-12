eaeb38969b1024587246c9357093a00886f3e9dc
shashinherath <shashin.malinda@gmail.com>
2024-06-12 21:47:58 +0530

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from . import models
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Custom admin class
+class UserModel(UserAdmin):
+    pass
+
+admin.site.register(CustomUser, UserModel)
