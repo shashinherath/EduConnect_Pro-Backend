2a4b6531b0ecb22c1a5f467b29b92a74f3d334e8
shashinherath <shashin.malinda@gmail.com>
2024-06-12 12:48:55 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'admin_api',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
