27523fa47135b3e643e5c5d972d19a33578351dc
shashinherath <shashin.malinda@gmail.com>
2024-06-18 23:33:59 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('login', views.doLogin, name='login'),
+    path('logout', views.doLogout, name='logout'),
+    path('addAdmin', views.addAdmin, name='addAdmin'),
+    path('admin_api', views.admin_api, name='admin_api'),
+    path('admin_api/<str:pk>', views.admin_api, name='admin_api'),
 ]
