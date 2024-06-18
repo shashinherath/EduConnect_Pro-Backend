9b2bfbc3f13eb73c8a97fd48b097b5b20f07f600
shashinherath <shashin.malinda@gmail.com>
2024-06-18 23:33:10 +0530

    Update serializers.py

--- main/serializers.py
+++ main/serializers.py
@@ -1,2 +1,27 @@
 from rest_framework import serializers
 from . import models
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.CustomUser
+        fields = ['username', 'email', 'password', 'user_type']
+
+    def create(self, validated_data):
+        user = models.CustomUser.objects.create_user(**validated_data)
+        return user
+
+class AdminSerializer(serializers.ModelSerializer):
+    admin = CustomUserSerializer()
+
+    class Meta:
+        model = models.Admin
+        fields = ['id', 'admin', 'profile_pic', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('admin')
+        user = CustomUserSerializer.create(CustomUserSerializer(), validated_data=user_data)
+        admin, created = models.Admin.objects.update_or_create(admin=user, **validated_data)
+        return admin
+
+
