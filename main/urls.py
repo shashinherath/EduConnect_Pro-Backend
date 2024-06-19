3a3273b0d0cd5a03b343edd38a67d0440bd8f0a2
shashinherath <shashin.malinda@gmail.com>
2024-06-20 00:21:47 +0530

    Update urls

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('login', views.doLogin, name='login'),
     path('logout', views.doLogout, name='logout'),
-    path('addAdmin', views.addAdmin, name='addAdmin'),
+    path('admin_add', views.admin_add, name='admin_add'),
     path('admin_api', views.admin_api, name='admin_api'),
-    path('admin_api/<str:pk>', views.admin_api, name='admin_api'),
+    path('admin_detail/<int:pk>', views.admin_detail, name='admin_detail'),
 ]
