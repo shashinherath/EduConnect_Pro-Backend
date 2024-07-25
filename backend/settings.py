a08fff5bfa46e1e9ea56b0c59562cbf3dd18718c
shashinherath <shashin.malinda@gmail.com>
2024-07-25 19:36:56 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-""" CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
 
 DATABASES = {
@@ -100,9 +100,9 @@ DATABASES = {
         "USER": CONNECTION_STR['user'],
         "PASSWORD": CONNECTION_STR['password'],
     }
-} """
+}
 
-DATABASES = {
+""" DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': os.environ['PGDATABASE'],
@@ -114,7 +114,7 @@ DATABASES = {
       'sslmode': 'require',
     },
   }
-}
+} """
 
 # OpenAI API Key
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
